8f602df9d73da6e7bd8b19672bdb4e027a96ae18
godlesie <j.godlesie@gmail.com>
2018-09-18 14:13:02 +0200

    Initial commit

---
+++ app/src/main/java/eu/godlesie/jgdlws/russianschnapsen_scorepointscounting/RussianSchnapsen.java
@@ -0,0 +1,13 @@
+package eu.godlesie.jgdlws.russianschnapsen_scorepointscounting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RussianSchnapsen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_russian_schnapsen);
+    }
+}
