ce01f9b0c36e9d435635827a36d90e56b037cd88
godlesie <j.godlesie@gmail.com>
2018-09-23 21:46:00 +0200

    Baza danych

---
+++ app/src/main/java/eu/godlesie/jgdlws/russianschnapsen_scorepointscounting/database/ThousendDatabase.java
@@ -0,0 +1,24 @@
+package eu.godlesie.jgdlws.russianschnapsen_scorepointscounting.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Rozgrywka.class,Gra.class}, version = 1)
+public abstract class ThousendDatabase extends RoomDatabase {
+    public abstract ThousenDao thousenDao();
+    private static volatile ThousendDatabase INSTANCE;
+    static ThousendDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (ThousendDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            ThousendDatabase.class,"thousend")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
